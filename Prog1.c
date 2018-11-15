7baff536b4827737a35b15fa2e7a81574bd26618
samara <samara@Samaras-MacBook-Pro.local>
2018-11-15 21:11:22 -0200

    bla

--- Prog1.c
+++ Prog1.c
@@ -1,14 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 // Programa 1
 // campos de tamanho fixo em bytes + registros de tamanho fixo em bytes
 typedef struct {
-	char key[2]
-	char lastname[20]
-	char firstname[20]
-	char address[20]
-	char city[10]
-	char state[10]
-	char zip[10]
-	char phone[14]
+	char key[2];
+	char lastname[20];
+	char firstname[20];
+	char address[20];
+	char city[10];
+	char state[10];
+	char zip[10];
+	char phone[14];
 } person;
 
 person p;
