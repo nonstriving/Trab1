cdedb77a8332bff8f060832f032b5c8d98be24aa
samara <samara@Samaras-MacBook-Pro.local>
2018-11-15 19:22:11 -0200

    first commit

---
+++ Prog1.c
@@ -0,0 +1,19 @@
+// Programa 1
+// campos de tamanho fixo em bytes + registros de tamanho fixo em bytes
+
+typedef struct {
+	char key[2]
+	char lastname[20]
+	char firstname[20]
+	char address[20]
+	char city[10]
+	char state[10]
+	char zip[10]
+	char phone[14]
+} pessoa;
+
+pessoa p;
+
+void lerdados(FILE * dados){
+	
+}
